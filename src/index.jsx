d9dd11f678ad9c169ea93dd80053290e20b99e8d
Luucas51 <oudartlucas51@gmail.com>
2022-02-03 08:29:12 +0100

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import './styles/index.scss'
+
+const App = () => {
+
+
+    return (
+        <>
+        </>
+    );
+};
+
+reactDom.render(<App />, document.getElementById('root'));
